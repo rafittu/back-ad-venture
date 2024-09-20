af13599a2a9777b907257929929c266e9af1a722
rafittu <rafittu@outlook.com>
2024-09-20 11:10:04 -0300

    chore: setup app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+        DATABASE_URL: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
