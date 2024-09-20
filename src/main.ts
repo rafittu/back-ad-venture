131a07dede54a124d543384c38dcce4c42b7ddb4
rafittu <rafittu@outlook.com>
2024-09-20 11:06:10 -0300

    chore: setup app main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.use(helmet.hidePoweredBy());
+  app.use(helmet.contentSecurityPolicy());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.enableShutdownHooks();
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
