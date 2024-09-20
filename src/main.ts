82d26459d6d4e616b8e9f1bffaa3db02f7722863
rafittu <rafittu@outlook.com>
2024-09-20 11:57:14 -0300

    chore: setup global error exceptions

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
+import { HttpExceptionFilter } from './common/filter/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   app.use(helmet());
   app.use(helmet.hidePoweredBy());
