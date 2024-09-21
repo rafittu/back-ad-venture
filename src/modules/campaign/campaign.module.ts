6b31016f074c49a266684206136f87aa9312f0c7
rafittu <rafittu@outlook.com>
2024-09-21 18:42:22 -0300

    chore: update campaign module providers

--- src/modules/campaign/campaign.module.ts
+++ src/modules/campaign/campaign.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CampaignController } from './campaign.controller';
+import { CampaignRepository } from './repository/campaign.repository';
+import { CreateCampaignService } from './services/create-campaign.service';
+import { PrismaService } from '../../prisma.service';
 
 @Module({
   controllers: [CampaignController],
-  providers: [],
+  providers: [PrismaService, CampaignRepository, CreateCampaignService],
 })
 export class CampaignModule {}
