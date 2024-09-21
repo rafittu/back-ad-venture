d4d6384adb11ed9cfdc64de915be95262a543d36
rafittu <rafittu@outlook.com>
2024-09-21 19:02:12 -0300

    feat: private method to convert snake_case object to camelCase

--- src/modules/campaign/repository/campaign.repository.ts
+++ src/modules/campaign/repository/campaign.repository.ts
@@ -9,11 +9,26 @@ import { AppError } from '../../../common/errors/Error';
 export class CampaignRepository implements ICampaignRepository<Campaign> {
   constructor(private prisma: PrismaService) {}
 
+  private toCamelCase(snakeCaseObject: any): any {
+    const camelCaseObject: any = {};
+
+    for (const key in snakeCaseObject) {
+      if (snakeCaseObject.hasOwnProperty(key)) {
+        const camelCaseKey = key.replace(/_\w/g, (match) =>
+          match[1].toUpperCase(),
+        );
+        camelCaseObject[camelCaseKey] = snakeCaseObject[key];
+      }
+    }
+
+    return camelCaseObject;
+  }
+
   async create(data: ICreateCampaign) {
     try {
       const campaign = await this.prisma.campaign.create({ data });
 
-      return campaign;
+      return this.toCamelCase(campaign);
     } catch (error) {
       if (error.code === 'P2002') {
         throw new AppError(
