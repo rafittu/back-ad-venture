4d80bcdb18cf18fac1ab108fe9ccbc8f581b5ed9
rafittu <rafittu@outlook.com>
2024-09-22 11:17:26 -0300

    chore: type create campaign return

--- src/modules/campaign/repository/campaign.repository.ts
+++ src/modules/campaign/repository/campaign.repository.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { ICampaignRepository } from '../interfaces/repository.interface';
-import { Campaign } from '@prisma/client';
 import { PrismaService } from '../../../prisma.service';
-import { ICreateCampaign } from '../interfaces/campaign.interface';
+import { ICampaign, ICreateCampaign } from '../interfaces/campaign.interface';
 import { AppError } from '../../../common/errors/Error';
 
 @Injectable()
-export class CampaignRepository implements ICampaignRepository<Campaign> {
+export class CampaignRepository implements ICampaignRepository<ICampaign> {
   constructor(private prisma: PrismaService) {}
 
   private toCamelCase(snakeCaseObject: any): any {
@@ -24,7 +23,7 @@ export class CampaignRepository implements ICampaignRepository<Campaign> {
     return camelCaseObject;
   }
 
-  async create(data: ICreateCampaign) {
+  async create(data: ICreateCampaign): Promise<ICampaign> {
     try {
       const campaign = await this.prisma.campaign.create({ data });
 
