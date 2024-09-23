6a053e79b3954dff2ac56b18580927bba4ba244a
rafittu <rafittu@outlook.com>
2024-09-23 12:12:50 -0300

    fix: remove duplicated iso format validation

--- src/modules/campaign/dto/create-campaign.dto.ts
+++ src/modules/campaign/dto/create-campaign.dto.ts
@@ -1,11 +1,4 @@
-import {
-  IsEnum,
-  IsNotEmpty,
-  IsString,
-  IsDate,
-  MinDate,
-  IsISO8601,
-} from 'class-validator';
+import { IsEnum, IsNotEmpty, IsString, IsDate, MinDate } from 'class-validator';
 import { Type } from 'class-transformer';
 import { CampaignCategory } from '@prisma/client';
 
@@ -22,7 +15,6 @@ export class CreateCampaignDto {
 
   @IsDate()
   @Type(() => Date)
-  @IsISO8601({ strict: true })
   @MinDate(new Date(), {
     message: `Campaign 'startDate' must be at present or future in ISO 8601 format.`,
   })
@@ -30,7 +22,6 @@ export class CreateCampaignDto {
 
   @IsDate()
   @Type(() => Date)
-  @IsISO8601({ strict: true })
   @MinDate(new Date(), {
     message: `Campaign 'endDate' must be at future in ISO 8601 format.`,
   })
