27bae07929ecdb3c769f452c878dde79a2b28bc4
rafittu <rafittu@outlook.com>
2024-09-23 16:27:07 -0300

    chore: update campaign module providers

--- src/modules/campaign/campaign.module.ts
+++ src/modules/campaign/campaign.module.ts
@@ -3,9 +3,15 @@ import { CampaignController } from './campaign.controller';
 import { CampaignRepository } from './repository/campaign.repository';
 import { CreateCampaignService } from './services/create-campaign.service';
 import { PrismaService } from '../../prisma.service';
+import { FindCampaignsByFilterService } from './services/find-campaigns-by-filter.service';
 
 @Module({
   controllers: [CampaignController],
-  providers: [PrismaService, CampaignRepository, CreateCampaignService],
+  providers: [
+    PrismaService,
+    CampaignRepository,
+    CreateCampaignService,
+    FindCampaignsByFilterService,
+  ],
 })
 export class CampaignModule {}
