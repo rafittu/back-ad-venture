9edda823b399cc4b0e0602016806c8ca7880c962
rafittu <rafittu@outlook.com>
2024-09-23 17:48:59 -0300

    feat: delete campaign service

---
+++ src/modules/campaign/services/delete-campaign.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { CampaignRepository } from '../repository/campaign.repository';
+import { ICampaignRepository } from '../interfaces/repository.interface';
+import { AppError } from 'src/common/errors/Error';
+
+@Injectable()
+export class DeleteCampaignService {
+  constructor(
+    @Inject(CampaignRepository)
+    private readonly campaignRepository: ICampaignRepository<any>,
+  ) {}
+
+  async execute(campaignId: string): Promise<void> {
+    const existingCampaign = await this.campaignRepository.findOne(campaignId);
+
+    if (!existingCampaign) {
+      throw new AppError('campaign-service.delete', 404, 'Campaign not found');
+    }
+
+    return await this.campaignRepository.delete(campaignId);
+  }
+}
