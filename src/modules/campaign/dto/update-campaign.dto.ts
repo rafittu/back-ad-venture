7cbbeba5dd6c48df670c3b9e10967aa571075c4d
rafittu <rafittu@outlook.com>
2024-09-23 16:39:01 -0300

    feat: update campaign dto

--- src/modules/campaign/dto/update-campaign.dto.ts
+++ src/modules/campaign/dto/update-campaign.dto.ts
@@ -1,4 +1,35 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCampaignDto } from './create-campaign.dto';
+import { IsOptional, IsString, IsEnum, MinDate } from 'class-validator';
+import { CampaignCategory, CampaignStatus } from '@prisma/client';
+import { Type } from 'class-transformer';
 
-export class UpdateCampaignDto extends PartialType(CreateCampaignDto) {}
+export class UpdateCampaignDto {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsEnum(CampaignStatus, {
+    message: `Campaign 'status' must be one of: ACTIVE, PAUSED, EXPIRED`,
+  })
+  status?: CampaignStatus;
+
+  @IsOptional()
+  @IsEnum(CampaignCategory, {
+    message: 'Campaign category must be one of the predefined values',
+  })
+  category?: CampaignCategory;
+
+  @IsOptional()
+  @Type(() => Date)
+  @MinDate(new Date(), {
+    message: `Campaign 'startDate' must be at present or future in ISO 8601 format.`,
+  })
+  startDate?: string;
+
+  @IsOptional()
+  @Type(() => Date)
+  @MinDate(new Date(), {
+    message: `Campaign 'endDate' must be at future in ISO 8601 format.`,
+  })
+  endDate?: string;
+}
