449f2a30acbcec9d58d9c17f43777235a66d19b6
rafittu <rafittu@outlook.com>
2024-09-23 16:26:41 -0300

    feat: find campaigns by filters repository

--- src/modules/campaign/interfaces/repository.interface.ts
+++ src/modules/campaign/interfaces/repository.interface.ts
@@ -1,5 +1,9 @@
-import { ICreateCampaign } from '../interfaces/campaign.interface';
+import {
+  CampaignFilters,
+  ICreateCampaign,
+} from '../interfaces/campaign.interface';
 
 export interface ICampaignRepository<ICampaign> {
   create(data: ICreateCampaign): Promise<ICampaign>;
+  findByFilters(filters: CampaignFilters): Promise<ICampaign[]>;
 }
--- src/modules/campaign/repository/campaign.repository.ts
+++ src/modules/campaign/repository/campaign.repository.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { ICampaignRepository } from '../interfaces/repository.interface';
 import { PrismaService } from '../../../prisma.service';
-import { ICampaign, ICreateCampaign } from '../interfaces/campaign.interface';
+import {
+  CampaignFilters,
+  ICampaign,
+  ICreateCampaign,
+} from '../interfaces/campaign.interface';
 import { AppError } from '../../../common/errors/Error';
 
 @Injectable()
@@ -23,6 +27,12 @@ export class CampaignRepository implements ICampaignRepository<ICampaign> {
     return camelCaseObject;
   }
 
+  private toCamelCaseArray(snakeCaseObjects: any[]): any[] {
+    return snakeCaseObjects.map((snakeCaseObject) =>
+      this.toCamelCase(snakeCaseObject),
+    );
+  }
+
   async create(data: ICreateCampaign): Promise<ICampaign> {
     try {
       const campaign = await this.prisma.campaign.create({ data });
@@ -41,6 +51,31 @@ export class CampaignRepository implements ICampaignRepository<ICampaign> {
         'campaign-repository.create',
         500,
         'campaign not created',
+      );
+    }
+  }
+
+  async findByFilters(filters: CampaignFilters): Promise<ICampaign[]> {
+    const { name, status, start_date, end_date } = filters;
+
+    try {
+      const campaigns = await this.prisma.campaign.findMany({
+        where: {
+          ...(name && {
+            name: { contains: name, mode: 'insensitive' },
+          }),
+          ...(status && { status: status }),
+          ...(start_date && { start_date: { gte: start_date } }),
+          ...(end_date && { end_date: { lte: end_date } }),
+        },
+      });
+
+      return this.toCamelCaseArray(campaigns);
+    } catch (error) {
+      throw new AppError(
+        'campaign-repository.findByFilters',
+        500,
+        'could not get campaigns',
       );
     }
   }
