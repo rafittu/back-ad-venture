c807dc5337f8b26c3ab1b36548c4e5b4ac697306
rafittu <rafittu@outlook.com>
2024-09-24 14:51:36 -0300

    feat: move cron job to his own service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CampaignModule } from './modules/campaign/campaign.module';
 import * as Joi from 'joi';
-import { Cron, ScheduleModule } from '@nestjs/schedule';
-import { ICampaignRepository } from './modules/campaign/interfaces/repository.interface';
-import { ICampaign } from './modules/campaign/interfaces/campaign.interface';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
@@ -24,13 +22,4 @@ import { ICampaign } from './modules/campaign/interfaces/campaign.interface';
   controllers: [],
   providers: [],
 })
-export class AppModule {
-  constructor(
-    private readonly campaignRepository: ICampaignRepository<ICampaign>,
-  ) {}
-
-  @Cron('0 * * * *')
-  async handleCron() {
-    await this.campaignRepository.checkCampaignStatus();
-  }
-}
+export class AppModule {}
